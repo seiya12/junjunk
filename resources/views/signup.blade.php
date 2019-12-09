cb4208573bd8f6227929a8803d59be95aa6b21e4
hirokikondo86 <kondo.h.0806@gmail.com>
2019-12-10 03:49:46 +0900

    Signupの追加

---
+++ resources/views/signup.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.user')
+
+<!-- head -->
+@section('title', 'Login')
+@section('style')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+<link rel="stylesheet" href="{{asset('css/bootstrap-social-gh-pages/bootstrap-social.css')}}" />
+<link rel="stylesheet" href="{{asset('css/signup.css')}}" />
+@endsection
+@include('common.head')
+
+<!-- header -->
+@include('common.user.header')
+
+<!-- content -->
+@section('content')
+<h1 id="title">新規会員登録</h1>
+<div id="signup">
+    <button type="button" class="btn btn-primary"><span class="fa fa-envelope"></span>メールで登録</button>
+    <button type="button" class="btn btn-danger"><span class="fa fa-google"></span>Googleで登録</button>
+    <button type="button" class="btn btn-dark"><span class="fa fa-github"></span>GitHubで登録</button>
+</div>
+@endsection
+
+<!-- footer -->
+@include('common.footer')
