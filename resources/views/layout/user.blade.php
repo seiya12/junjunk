9f9f293cd1f45370b523f3e106eac05c3004e654
hirokikondo86 <kondo.h.0806@gmail.com>
2019-12-10 03:46:35 +0900

    Loginの追加

---
+++ resources/views/layout/user.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    @yield('head')
+</head>
+
+<body>
+    <div id="wrapper">
+        @yield('header')
+        @yield('content')
+        @yield('footer')
+
+        <!-- JavaScript -->
+        <script src="{{ asset('js/app.js') }}"></script>
+    </div>
+</body>
+
+</html>
