b0705ca20381651d3e42bf5a7d04da5c7225eed7
hirokikondo86 <kondo.h.0806@gmail.com>
2019-12-14 02:32:26 +0900

    namespaceの修正

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
-use App\User;
+use App\Models\User;
 use Socialite;
 use Hash;
 
