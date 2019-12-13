6f24f62ac0111c59b147dee97e0aab442e657b76
seiya12 <t72txe6n@i.softbank.jp>
2019-12-13 11:57:18 +0900

    migration users,products

--- database/migrations/2019_12_10_052921_create_products_table.php
+++ database/migrations/2019_12_10_052921_create_products_table.php
@@ -15,6 +15,12 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->char('product_code', 12)->unique();
+            $table->char('exhibit_user_code',9);
+            $table->char('product_category',2);
+            $table->integer('product_price');
+            $table->char('exhibit_type', 1);
+            $table->dateTime('end_date');
             $table->timestamps();
         });
     }
