b264f7668b68f911c73fb8fd775ad6cf8e9cc717
hirokikondo86 <kondo.h.0806@gmail.com>
2019-12-21 02:13:49 +0900

    migrationの追加

---
+++ database/migrations/2019_12_20_171205_create_category_histories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryHistories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_histories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('category');
+            $table->integer('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_histories');
+    }
+}
