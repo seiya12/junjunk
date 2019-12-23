dc2b38b136de5603bb4c99ac24d65a32ca27ad0c
hirokikondo86 <kondo.h.0806@gmail.com>
2019-12-24 01:23:50 +0900

    Authを継承したUserモデルにContracts\Authを実装

--- app/Model/User.php
+++ app/Model/User.php
@@ -5,7 +5,7 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements \Illuminate\Contracts\Auth\Authenticatable
 {
     protected $fillable = [
         'user_code',
