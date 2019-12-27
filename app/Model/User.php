59e922053696c79719ceb10ea0646f2dd0f4c093
EC2 Default User <ec2-user@ip-10-0-2-25.us-east-2.compute.internal>
2019-12-27 16:45:46 +0000

    test

--- app/Model/User.php
+++ app/Model/User.php
@@ -3,8 +3,9 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable implements \Illuminate\Contracts\Auth\Authenticatable
 {
     protected $fillable = [
         'user_code',
