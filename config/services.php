9d9268d1aec4531c47f520832c962a8df528539a
hirokikondo86 <kondo.h.0806@gmail.com>
2020-01-17 23:55:00 +0900

    Modeify callback url

--- config/services.php
+++ config/services.php
@@ -37,13 +37,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => env('APP_URL') . '/login/google/callback',
+        'redirect' => env('APP_URL') . ':8000/login/google/callback',
     ],
 
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => env('APP_URL') . '/login/github/callback',
+        'redirect' => env('APP_URL') . ':8000/login/github/callback',
     ],
 
     'stripe' => [
