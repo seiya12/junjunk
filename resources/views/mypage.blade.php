acd9671952d440aadd35e8fc2e0514f4eee9f9d9
seiya12 <t72txe6n@i.softbank.jp>
2020-01-18 21:59:27 +0900

    間違ってmasterで作業してたからとりあえず

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -12,7 +12,25 @@
 
 <!-- content -->
 @section('content')
-<h1>MyPage</h1>
+<div id="cont">
+  <div id="name">{{ $user->account_name }}</div>
+  <h1>個人情報設定</h1>
+
+  <h1>出品した商品</h1>
+  <ul>
+  @foreach ($sells as $sell)
+    <li>{{ $sell->product_code }}</li>
+    <li>{{ $sell->name }}</li>
+  @endforeach
+  </ul>
+  <h1>購入した商品</h1>
+  <ul>
+  @foreach ($buys as $buy)
+    <li>{{ $buy->product_code }}</li>
+    <li>{{ $buy->name }}</li>
+  @endforeach
+  </ul>
+</div>
 @endsection
 
 <!-- footer -->
