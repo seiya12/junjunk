e40a2a3ea1f1e9ec628a0c06c1de83a8b30f5e14
seiya12 <t72txe6n@i.softbank.jp>
2020-01-19 19:58:48 +0900

    とりあえず

--- app/Http/Controllers/BuyController.php
+++ app/Http/Controllers/BuyController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Model\Product;
+use App\Model\User;
+use App\Model\Warehouse;
+use App\Model\Transaction;
 use Stripe\Stripe;
 use Stripe\Charge;
+use Auth;
 
 class BuyController extends Controller
 {
@@ -30,11 +34,46 @@ class BuyController extends Controller
         ));
 
         // productテーブルから論理削除
-        Product::where('product_code',$req->product_code)->delete();
+        // Product::where('product_code',$req->product_code)->delete();
 
         // transaction書き込み
-        dd($req->sell_user_code);
+        // $sells =  Product::withTrashed()
+        // ->join('users', 'users.user_code', '=', 'products.sell_user_code')
+        // ->where('user_code',$user->user_code)
+        // ->select('product_code','products.name')
+        // ->get();
+        $warehouse_code = User::join('users.prefecture','=','warehouses','warehouses.prefecture')
+            ->where('users.user_code','=',$req->sell_user_code)
+            ->select('warehouse_code');
+        
+        dd($warehouse_code);
+        // $transaction_code =  $this->createTransactionCode($warehouse_code);
+        // $user_code = Auth::user();
+        // Transaction::insert([
+        //     'transaction_code' => $transaction_code,
+        //     'seller_code'      => $req->sell_user_code,
+        //     'buyer_code'       => $user_code,
+        //     'product_code'     => $req->product_code,
+        //     'warehouse_code'   => $warehouse_code,
+        // ]);
 
+        // transaction番号出力
         return redirect()->route('top');
+    }
+
+    public function createTransactionCode($warehouse_code)
+    {
+        $section = 'F';
+        $year = str_pad(date('y'), 3, 0, STR_PAD_LEFT);
+        $month = strtoupper(dechex(date('n')));
+        $cnt = Product::where('product_code', 'LIKE', "$warehouse_code$section$year$month%")->count();
+        if ($cnt === 0) {
+            $num = str_pad(1, 5, 0, STR_PAD_LEFT);
+        } else {
+            $num = str_pad($cnt + 1, 5, 0, STR_PAD_LEFT);
+        }
+        $productCode = $category . $section . $year . $month . $num;
+
+        return $productCode;
     }
 }
