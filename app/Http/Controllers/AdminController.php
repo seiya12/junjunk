893e468ee58f6c307c486ef79955c0aa28a3ce7e
seiya12 <t72txe6n@i.softbank.jp>
2020-01-25 21:44:28 +0900

    本日本月の利益表示

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,29 +12,10 @@ class AdminController extends Controller
 {
     public function index()
     {
-        // dd(Auth::user());
-        $user = Auth::user();
-        // dd($user->name);
-        // $sells =  Product::withTrashed()
-        //     ->join('users', 'users.user_code', '=', 'products.sell_user_code')
-        //     ->join('transactions', 'transactions.product_code', '=', 'products.product_code')
-        //     ->where('user_code',$user->user_code)
-        //     ->select('products.product_code','products.nam','products.deleted_at','products.price','transactions.transaction_code','transactions.status','users.user_code')
-        //     // ->get();
-        //     ->paginate(20, ["*"], 'sells')
-        //     ->appends(["itempage" => Input::get('buys')]);
-        $sells =  Product::withTrashed()
-            ->join('users', 'users.user_code', '=', 'products.sell_user_code')
-            ->leftJoin('transactions','transactions.product_code','=','products.product_code')
-            ->where('user_code',$user->user_code)
-            ->select('products.product_code','products.name','products.deleted_at','products.price','users.user_code','transactions.transaction_code','transactions.status')
-            ->paginate(3, ["*"], 'sells')
-            ->appends(["itempage" => Input::get('buys')]);
-        $buys =  Transaction::join('products', 'products.product_code', '=', 'transactions.product_code')
-            ->where('buyer_code',$user->user_code)
-            ->select('products.product_code','products.name','products.sell_user_code','products.price','transactions.status','transactions.transaction_code')
-            ->paginate(3, ["*"], 'buys')
-            ->appends(["itempage" => Input::get('sells')]);
-        return view('admin', compact('user', 'sells','buys'));
+        $todayCount =  Transaction::whereDate('created_at', '=', date('Y-m-d'))
+            ->count();
+        $todayProfit = Transaction::whereDate('created_at', '=', date('Y-m-d'))
+            ->sum('profit');
+        return view('admin', compact('todayCount','todayProfit'));
     }
 }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -12,83 +12,47 @@
 
 <!-- content -->
 @section('content')
-<div id="name">{{ $user->account_name }}</div>
+<div id="title">営業成績表示</div>
 <div id="cont">
-  <h1>出品した商品</h1>
-  <ul>
-  @foreach ($sells as $sell)
-    <!-- <li>{{ $sell->product_code }}</li> --> 
-    <div class="productBox">
-      <a href="/product/{{ $sell->product_code }}">
-        <li class="productLi">
-          <img src="https://junjunk.s3-ap-northeast-1.amazonaws.com/{{ $user['user_code'] }}/{{ $sell['product_code'] }}_1.jpg" alt="商品画像">
-          <div class ="productCont">
-            <p>{{ $sell->name }}</p>
-            <p>¥{{ number_format($sell->price) }}</p>
-            @if ($sell->deleted_at == null)
-              <p class="green">出品中</p>
-            @else
-              <p class="red">販売済み</p>
-            @endif
-            @if ($sell->status == 1)
-              <p class="green">発送待ち</p>
-              <p class="transaction">ID:{{ $sell->transaction_code }}</p>
-            @elseif ($sell->status == 2)
-              <p class="red">取引完了</p>
-              <p class="transaction">ID:{{ $sell->transaction_code }}</p>
-            @else
-              <br><br>
-            @endif
-            <!-- <p>{{ $sell->transaction_code }}</p> -->
-          </div>
-        </li>
-      </a>
-    </div>
-  @endforeach
-  </ul>
-  <div class="d-flex justify-content-center">
-    {{ $sells->links() }}
-  </div>
-  <h1>購入した商品</h1>
-  <ul>
-  @foreach ($buys as $buy)
-    <!-- <li>{{ $buy->product_code }}</li> -->
-    <div class="productBox">
-    <a href="/product/{{ $buy->product_code }}"><li class="productLi">
-      <img src="https://junjunk.s3-ap-northeast-1.amazonaws.com/{{ $buy['sell_user_code'] }}/{{ $buy['product_code'] }}_1.jpg" alt="商品画像">
-      <div class="productCont">
-        <p>{{ $buy->name }}</p>
-        <p>¥{{ number_format($buy->price) }}</p>
-        @if ($buy->status == 1)
-          <p class="green">発送待ち</p>
-        @else
-          <p class="red">取引完了</p>
-        @endif
-        <p class="transaction">ID:{{ $buy->transaction_code }}</p>
-      </div>
-    </li></a></div>
-  @endforeach
-  </ul>
-  <div class="d-flex justify-content-center">
-    {{ $buys->links() }}
-  </div>
-
-  <h1>アカウント設定</h1>
-  <form action="#">
-    <label>メールアドレス</label>
-    <p><input type="email" name ="email" class="form-control" value="{{ $user->email }}"></p>
-    <label>アカウント名</label>
-    <p><input type="text" name ="account_name" class="form-control" value="{{ $user->account_name }}"></p>
-    <label>氏名</label>
-    <p><input type="text" name ="name" class="form-control" value="{{ $user->name }}"></p>
-    <label>郵便番号</label>
-    <p><input type="text" name ="postal_code" class="form-control" value="{{ $user->postal_code }}"></p>
-    <label>都道府県</label>
-    <p><input type="text" name ="prefectures" class="form-control" value="{{ $user->prefectures }}"></p>
-    <label>住所</label>
-    <p><input type="text" name ="street_address" class="form-control" value="{{ $user->street_address }}"></p>
-    <a href=""><button type="submit" class="btn btn-danger">変更する</button></a>
-  </form>
+  <h1>本日の成績<span>({{ date('Y年m月d日') }})</span></h1>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">項目</th>
+        <th scope="col">成績</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>取引件数</td>
+        <td>{{ $todayCount }}件</td>
+      </tr>
+      <tr>
+        <td>利益</td>
+        <td>{{ number_format($todayProfit) }}円</td>
+      </tr>
+    </tbody>
+  </table>
+  
+  <h1>今月の成績<span>({{ date('Y年m月') }})</span></h1>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">項目</th>
+        <th scope="col">成績</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>取引件数</td>
+        <td>{{ $todayCount }}件</td>
+      </tr>
+      <tr>
+        <td>利益</td>
+        <td>{{ number_format($todayProfit) }}円</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 @endsection
 <!-- footer -->
