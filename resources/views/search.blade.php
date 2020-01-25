07a64a063aae5d8f9283e7547f62beadd4602ff3
gimlet <gimlet.koban@gmail.com>
2020-01-25 22:31:13 +0900

    edit search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -78,6 +78,7 @@
             <li><img src="https://junjunk.s3-ap-northeast-1.amazonaws.com/{{ $product['sell_user_code'] }}/{{ $product['product_code'] }}_1.jpg" alt="商品画像"></li>
             <li class="height">{{ $product['name'] }}</li>
             <li class="price">¥{{ number_format($product['price']) }}</li>
+            <li class="method">フリマ</li>
         </a>
       </div><!-- search_item -->
       @endforeach
